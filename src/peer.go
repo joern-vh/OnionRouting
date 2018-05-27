e953e9391265b97e5b74a8f5e08d7708edcf79ae
Jörn von Henning <joern.von.henning@tum.de>
2018-05-27 15:19:22 +0200

    Implented TCP-Listener.

--- src/peer.go
+++ src/peer.go
@@ -1,1 +1,39 @@
-package src
+package main
+
+import (
+	"net"
+	"bufio"
+	"fmt"
+	"log"
+)
+
+func listen(port string) {
+	fmt.Printf("Started listening on port %s:\n\n", port)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", ":" + port)
+	if err != nil {
+		log.Println(err)
+	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+
+		}
+		fmt.Printf("New Message: %s\n", message)
+	}
+}
+
+func main() {
+	listen("3000")
+}
