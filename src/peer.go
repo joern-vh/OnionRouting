bcd73ed7d7bab2a5941b1419530534fa6e29c5a7
Jan-Cedric Anslinger <jc.anslinger@mintme.io>
2018-06-06 23:24:43 +0200

    Added functionality to start a new peer and listen for new TCP connections

--- src/peer.go
+++ src/peer.go
@@ -1,49 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"log"
-	"net"
-	"errors"
-	"os"
+	"services"
 )
 
-const destinationAddress = "192.168.0.15:3000"
-//const destinationAddress = "127.0.0.1:3000"
-
-// Open returns a new tcp connection inside a bufio read writer
-func open() (*bufio.ReadWriter, error) {
-
-	log.Println("Dial " + destinationAddress)
-	// First, resolve TCP address
-	tcpAddr, err := net.ResolveTCPAddr("tcp", destinationAddress)
-	if err != nil {
-		return nil, errors.New("ResolveTCPAddr failed: " + err.Error())
-	}
-
-	// Use tcp here for all "structure like" requests, traffic later over UDP
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		return nil, errors.New("Dial failed: " + err.Error())
-	}
-
-	return bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn)), nil
-}
-
 func main() {
-	conn, err := open()
+	log.Println("Start creating peer")
+
+	// Start creating a peer which can just start listening
+	newPeer, err := services.CreateNewPeer(3000, "127.0.0.1")
 	if err != nil {
-		log.Println("Failed opening a connection" + destinationAddress)
-		log.Println("Error: ", err)
-		os.Exit(0)
+		log.Println("Couldn't create new Peer, error:", err)
 	}
 
-	n, err := conn.WriteString("It's not a bug, it's a feature !!!!!!! \n")
-	if err != nil {
-		log.Println("Failed sending to other peer")
-		log.Println("Error: ", err)
+	// Now start listening
+	if err := newPeer.StartTCPListening(); err != nil {
+		log.Println("Problem listening for new messages: ", err.Error())
+		log.Println("Stopped peer due to error")
+		return
 	}
-
-	err = conn.Flush()
-	log.Println("N: ", n)
 }
