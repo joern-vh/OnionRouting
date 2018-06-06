a3ecede26b1afed88ffc01a555cdef2afedcf078
Jan-Cedric Anslinger <jc.anslinger@mintme.io>
2018-06-07 00:25:47 +0200

    Created config model to save the configuration in

---
+++ src/models/config_model.go
@@ -0,0 +1,7 @@
+package models
+
+// Model used to parse all necessary information out of an config.ini file TODO: Adapt later to actual config file, now just testing
+type Config struct {
+	P2P_Port		int				`json:"p2p_port"`
+	P2P_Hostname	string			`json:"p2p_hostname"`
+}
