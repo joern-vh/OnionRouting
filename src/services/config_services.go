c05869272818c54ffbc9fffdd59c98d408e9800b
Jan-Cedric Anslinger <jc.anslinger@mintme.io>
2018-06-07 12:27:51 +0200

    Added ParseStartFlags() to handle flags given when starting a new peer

--- src/services/config_services.go
+++ src/services/config_services.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"github.com/Thomasdezeeuw/ini"
 	"strconv"
+	"flag"
 )
 
 // NewConfigObject creates a new config struct based on an config.ini file, passed as parameter
@@ -30,4 +31,40 @@ func NewConfigObject(pathToFile string) (*models.Config, error) {
 	}
 
 	return &models.Config{configP2PPort, config["onion"]["p2p_hostname"]}, nil
+}
+
+// ParseStartFlags check whether one of the available flags is used
+func ParseStartFlags() (string, bool, string, int, error){
+	// Define and parse command line flags
+	configFileFlag := flag.String("c", "config.ini", "Please write the path to your config.ini file ")
+	sendMessage := flag.Bool("message", false, "Use parameter m to show that we need parameter h & p")
+	sendMessageHost := flag.String("h", "127.0.0.1", "Please enter the ip address of your destination host")
+	sendMessagePort := flag.String("p", "127.0.0.1", "Please enter the ip address of your destination host")
+	flag.Parse()
+
+	// Check if path was empty
+	if *configFileFlag == "" {
+		flag.PrintDefaults()
+		return "", false, "", 0, errors.New("ParseStartFlags: No config.ini path was provided")
+	}
+
+	// Check if we need to send a message
+	if *sendMessage == true {
+		// Check that port and host aren't empty
+		if *sendMessageHost == "" || *sendMessagePort == "" {
+			flag.PrintDefaults()
+			return "", false, "", 0, errors.New("ParseStartFlags: Please provide an ip and a port when sending a message")
+		}
+
+		// Convert sendMessagePort to int
+		sendMessagePortInt, err := strconv.Atoi(*sendMessagePort)
+		if err != nil {
+			flag.PrintDefaults()
+			return "", false, "", 0, errors.New("ParseStartFlags: Please provide a valid port as number(int)")
+		}
+
+		return *configFileFlag, *sendMessage, *sendMessageHost, sendMessagePortInt, nil
+	}
+
+	return *configFileFlag, *sendMessage, "", 0, nil
 }
