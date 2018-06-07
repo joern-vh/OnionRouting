3e39f8bde209d924e1e3f924fb6e951e2761e498
Jan-Cedric Anslinger <jc.anslinger@mintme.io>
2018-06-07 12:27:02 +0200

    Created a goroutine to run the listen function non blocking and added ParseStartFlags() to handle parameters when starting the peer

--- src/peer.go
+++ src/peer.go
@@ -3,23 +3,22 @@ package main
 import (
 	"services"
 	"log"
-	"flag"
 	"os"
+	"time"
+	"models"
 )
 
 func main() {
-	// Define and parse command line flags
-	possibleFlags := flag.String("c", "config.ini", "Pleas write the path to your config.ini file ")
-	flag.Parse()
 
-	// Check if path was empty
-	if *possibleFlags == "" {
-		flag.PrintDefaults()
+	// Get all start-flags
+	configFileFlag, sendMessage, sendMessageHost, sendMessagePort, err := services.ParseStartFlags()
+	if err != nil {
+		log.Println("Error parsing all start-flags: " + err.Error())
 		os.Exit(1)
 	}
 
 	// Create new config file based on read in path
-	config, err := services.NewConfigObject(*possibleFlags)
+	config, err := services.NewConfigObject(configFileFlag)
 	if err != nil {
 		log.Print("Couldn't parse config file, please check it")
 		os.Exit(1)
@@ -31,18 +30,34 @@ func main() {
 		log.Println("Couldn't create new Peer, error:", err)
 	}
 
-	// Now start listening
-	if err := newPeer.StartTCPListening(); err != nil {
-		log.Println("Problem listening for new messages: ", err.Error())
-		log.Println("Stopped peer due to error")
-		return
+	// This need to run concurrent, so that we can listen and write at the same time >> Use channel for synchronisation, use gofunc to run concurrent
+	communicationChannel := make(chan error)
+	go func() {
+		// Now start listening
+		if err := newPeer.StartTCPListening(); err != nil {
+			log.Println("Problem listening for new messages: ")
+			log.Println("Stopped peer due to error")
+			communicationChannel <- err
+			return
+		}
+	}()
+
+	// Sleep for two seconds to give StartTCPListening() time to create the TCPListener
+	time.Sleep(time.Second * 2)
+
+	// For testing, check if we need to send a message and if yes, build and send it
+	if sendMessage == true {
+		log.Println("Create message")
+		buildMessage := models.OnionTunnelBuild{OnionTunnelBuild: uint16(560), NetworkVersion: "IPv4", Port: uint16(4200), DestinationAddress: "", DestinationHostkey: "KEY"}
+		onionTunnelBuild := services.CreateOnionTunnelBuild(buildMessage)
+		log.Printf("Message: %x\n", onionTunnelBuild)
+		newPeer.SendMessage(sendMessageHost, sendMessagePort, onionTunnelBuild)
 	}
 
-	// TODO: Write new parameter option to send something
-	/*
-	buildMessage := models.OnionTunnelBuild{OnionTunnelBuild: uint16(560), NetworkVersion: "IPv4", Port: uint16(4200), DestinationAddress: "", DestinationHostkey: "KEY"}
-	onionTunnelBuild := services.CreateOnionTunnelBuild(buildMessage)
-	fmt.Printf("Message: %x\n", onionTunnelBuild)
-	sendMessage("192.168.0.10", 3000, onionTunnelBuild)
-	 */
+	// Wait here for errors from the channel
+	if err := <- communicationChannel; err != nil {
+		log.Println("Yes, error")
+		log.Println(err.Error())
+		os.Exit(1)
+	}
 }
