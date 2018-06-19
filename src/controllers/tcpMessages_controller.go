349474442ad4853d8a5df90f6e06e9d0807ae2d3
Jörn von Henning <joern.von.henning@tum.de>
2018-06-19 22:42:00 +0200

    Outsourced handle message functions.

--- src/controllers/tcpMessages_controller.go
+++ src/controllers/tcpMessages_controller.go
@@ -4,7 +4,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"log"
-	"fmt"
 	"net"
 	"services"
 
@@ -22,74 +21,22 @@ func  handleTCPMessage(message []byte, myPeer *services.Peer) error {
 	switch messageType {
 		// ONION TUNNEL BUILD
 		case 560:
-			var networkVersionString string
-			var destinationAddress string
-			var destinationHostkey []byte
-
-			networkVersion := binary.BigEndian.Uint16(message[4:6])
-			onionPort := binary.BigEndian.Uint16(message[6:8])
-
-			if networkVersion == 0 {
-				networkVersionString = "IPv4"
-				destinationAddress = net.IP(message[8:12]).String()
-				destinationHostkey = message[12:]
-			} else if networkVersion == 1 {
-				networkVersionString = "IPv6"
-				destinationAddress = net.IP(message[8:24]).String()
-				destinationHostkey = message[24:]
-			}
-
-			// ToDo: Functionality.
-
-			log.Printf("Network Version: %s\n", networkVersionString)
-			log.Printf("Onion Port: %d\n", onionPort)
-			log.Printf("Destination Address: %s\n", destinationAddress)
-			log.Printf("Destination Hostkey: %s\n", destinationHostkey)
+			handleOnionTunnelBuild(message)
 			break
 
 		// ONION TUNNEL DESTROY
 		case 563:
-			log.Println("ONION TUNNEL DESTROY received")
-			tunnelID := string(message[4:8])
-			fmt.Printf("Tunnel ID: %s\n", tunnelID)
+			handleOnionTunnelDestroy(message)
 			break
 
 		// CONSTRUCT TUNNEL
 		case 567:
-			var networkVersionString string
-			var destinationAddress string
-			var destinationHostkey []byte
-
-			networkVersion := binary.BigEndian.Uint16(message[4:6])
-			onionPort := binary.BigEndian.Uint16(message[6:8])
-
-			if networkVersion == 0 {
-				networkVersionString = "IPv4"
-				destinationAddress = net.IP(message[8:12]).String()
-				destinationHostkey = message[12:]
-			} else if networkVersion == 1 {
-				networkVersionString = "IPv6"
-				destinationAddress = net.IP(message[8:24]).String()
-				destinationHostkey = message[24:]
-			}
-
-			// ToDo: Functionality.
-
-			log.Printf("Network Version: %s\n", networkVersionString)
-			log.Printf("Onion Port: %d\n", onionPort)
-			log.Printf("Destination Address: %s\n", destinationAddress)
-			log.Printf("Destination Hostkey: %s\n", destinationHostkey)
+			handleConstructTunnel(message)
 			break
 
 		// CONFIRM TUNNEL CONSTRUCTION
 		case 568:
-			onionPort := binary.BigEndian.Uint16(message[4:6])
-			tunnelID := string(message[6:10])
-			destinationHostkey := message[10:]
-
-			log.Printf("Onion Port: %s\n", onionPort)
-			log.Printf("Tunnel ID: %s\n", tunnelID)
-			log.Printf("Destination Hostkey: %s\n", destinationHostkey)
+			handleConfirmTunnelConstruction(message)
 			break
 
 		// ToDo: Handle Error Messages while construction is ongoing.
@@ -99,4 +46,72 @@ func  handleTCPMessage(message []byte, myPeer *services.Peer) error {
 	}
 
 	return nil
+}
+
+func handleOnionTunnelBuild(message []byte) {
+	var networkVersionString string
+	var destinationAddress string
+	var destinationHostkey []byte
+
+	networkVersion := binary.BigEndian.Uint16(message[4:6])
+	onionPort := binary.BigEndian.Uint16(message[6:8])
+
+	if networkVersion == 0 {
+		networkVersionString = "IPv4"
+		destinationAddress = net.IP(message[8:12]).String()
+		destinationHostkey = message[12:]
+	} else if networkVersion == 1 {
+		networkVersionString = "IPv6"
+		destinationAddress = net.IP(message[8:24]).String()
+		destinationHostkey = message[24:]
+	}
+
+	// ToDo: Functionality.
+
+	log.Printf("Network Version: %s\n", networkVersionString)
+	log.Printf("Onion Port: %d\n", onionPort)
+	log.Printf("Destination Address: %s\n", destinationAddress)
+	log.Printf("Destination Hostkey: %s\n", destinationHostkey)
+}
+
+func handleOnionTunnelDestroy(message []byte) {
+	log.Println("ONION TUNNEL DESTROY received")
+	tunnelID := string(message[4:8])
+	log.Printf("Tunnel ID: %s\n", tunnelID)
+}
+
+func handleConstructTunnel(message []byte) {
+	var networkVersionString string
+	var destinationAddress string
+	var destinationHostkey []byte
+
+	networkVersion := binary.BigEndian.Uint16(message[4:6])
+	onionPort := binary.BigEndian.Uint16(message[6:8])
+
+	if networkVersion == 0 {
+		networkVersionString = "IPv4"
+		destinationAddress = net.IP(message[8:12]).String()
+		destinationHostkey = message[12:]
+	} else if networkVersion == 1 {
+		networkVersionString = "IPv6"
+		destinationAddress = net.IP(message[8:24]).String()
+		destinationHostkey = message[24:]
+	}
+
+	// ToDo: Functionality.
+
+	log.Printf("Network Version: %s\n", networkVersionString)
+	log.Printf("Onion Port: %d\n", onionPort)
+	log.Printf("Destination Address: %s\n", destinationAddress)
+	log.Printf("Destination Hostkey: %s\n", destinationHostkey)
+}
+
+func handleConfirmTunnelConstruction(message []byte) {
+	onionPort := binary.BigEndian.Uint16(message[4:6])
+	tunnelID := string(message[6:10])
+	destinationHostkey := message[10:]
+
+	log.Printf("Onion Port: %s\n", onionPort)
+	log.Printf("Tunnel ID: %s\n", tunnelID)
+	log.Printf("Destination Hostkey: %s\n", destinationHostkey)
 }
