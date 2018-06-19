622777db7a1f36c8aafd66963ca2f64ce84e993b
Jörn von Henning <joern.von.henning@tum.de>
2018-06-19 23:02:04 +0200

    Initialized UDP Message Controller.

--- src/controllers/udpMessages_controller.go
+++ src/controllers/udpMessages_controller.go
@@ -1,1 +1,13 @@
 package controllers
+
+import "services"
+
+func StartUDPController(myPeer *services.Peer) {
+	for msg := range services.CommunicationChannelUDPMessages {
+		handleUDPMessage(msg, myPeer)
+	}
+}
+
+func handleUDPMessage(message []byte, myPeer *services.Peer) {
+	
+}
